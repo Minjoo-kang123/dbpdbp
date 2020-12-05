361ba5f4bad213e30e6d96e6753ffb38d0087d73
Jung-Laon <ho8181212@gmail.com>
2020-12-06 03:15:00 +0900

    Add files via upload

---
+++ DB/dummydata(add_img, add_url).sql
@@ -0,0 +1,240 @@
+insert into bookInfo values
+('9791165341909', '달러구트 꿈 백화점', '이미예', '팩토리나인', '한국 소설', 'img src', '5')
+
+insert into bookInfo values
+('9788936434267', '아몬드', '손원평', '창비', '한국 소설','img src', '7')
+
+insert into bookInfo values
+('9791190090018', '우리가 빛의 속도로 갈 수 없다면', '김초엽', '허블', 'SF 소설', 'img src', '2')
+
+insert into bookInfo values
+('9788954672214', '시선으로부터', '정세랑', '문학동네', '한국소설', 'img src', '4')
+
+insert into bookInfo values
+('9788954654753', '체리새우: 비밀글입니다', '황영미', '문학동네', '한국 소설', 'img src', '1')
+
+insert into bookInfo values
+('9791190090261', '천 개의 파랑', '천선란', '허블', 'sf소설', 'img src', '9')
+
+insert into bookInfo values
+('9791164136452', '공정하다는 착각', '마이클 샌델', '와이즈베리', '사회학','img src', '2')
+
+insert into bookInfo values
+('9788967358020', '매우 예민한 사람들을 위한 책', '전홍진', '글항아리', '교양 심리','img src', '4')
+
+insert into bookInfo values
+('9791190313131', '지적 대화를 위한 넓고 얕은 지식: 제로 편', '채사장', '웨일북', '인문 교양', 'img src', '0')
+
+insert into bookInfo values
+('9791187750390', '우리 시대 고전 읽기', '정승민', '눌민', '인문 교양', 'img src', '3')
+
+insert into bookInfo values
+('9791190242516', '존리의 금융문맹 탈출', '존 리', '베가북스', '경제', 'img src', '5')
+
+insert into bookInfo values
+('9788950992781', '노희영의 브랜딩 법칙', '노희영', '21세기북스', '경제', 'img src', '7')
+
+insert into bookInfo values
+('9788925599632', '규칙 없음', '리드헤이스팅, 에린 마이어', '알에이치코리아'. '경제', 'img src', '7')
+
+insert into bookInfo values
+('9791156121763', '체르노빌 생존 지침서', '케이트 브라운', '푸른역사', '역사', 'img src', '11')
+
+insert into bookInfo values
+('9791191119695', '왕의 공부', '김준태', '위즈덤하우스', '역사', 'img src', '2')
+
+insert into bookInfo values
+('9791188635108', '세계사를 바꾼 10가지 약', '사토 겐타로', '사람과나무사이', '역사', 'img src', '8')
+
+insert into bookInfo values
+('9788994606613','정의의 감정들', '김지수', '너머북스', '역사', 'img src', '15')
+
+insert into bookInfo values
+('9788954675987', '일인칭 단수', '무라카미 하루키', '문학동네', '일본 소설', 'img src', '7')
+
+insert into bookInfo values
+('9788984374164', '인생은 소설이다', '기욤 뮈소', '밝은세상', '프랑스 소설', 'img src', '4')
+
+insert into bookInfo values
+('9791185871134', '50 대 50', 'S. L. 파월', '라임', '영미 소설', 'img src', '2')
+
+insert into bookInfo values
+('9788954670302', '독고솜에게 반하면', '허진희', '문학동네', '한국 소설', 'img src', '9')
+
+insert into bookInfo values
+('9788932917207', '앵무새 죽이기', '하퍼 리', '열린책들', '영미소설', 'img src', '21')
+
+insert into bookInfo values
+('9788932909349', '그리스인 조르바', '니코스 카잔차키스', '열린책들', '유럽 소설', 'img src', '3')
+
+insert into bookInfo values
+('9788955824957', '내 영혼이 따뜻했던 날들', '포리스터 카터', '아름드리미디어', '영미 소설', 'img src', '13')
+
+insert into bookInfo values
+('9791190030717', '왜 나는 도와달라는 말을 못할까', '웨인 베이커', '어크로스', '자기계발', 'img src', '9')
+
+insert into bookInfo values
+('9791196776244', '비록 영업의 고수는 아니지만', '지찬솔', '훈훈', '자기계발', 'img src', '6')
+
+insert into bookInfo values
+('9791158511906', '나의 하루는 4시 30분에 시작된다', '김유진', '토네이도', '자기계발', 'img src', '27')
+
+insert into bookInfo values
+('9791189437039', '바쁘면 이책도 읽지 마세요', '도날드 로스', '사일런스북', '자기계발', 'img src', '8')
+
+insert into bookInfo values
+('9791196269494', '여자는 인질이다', '디 그레이엄, 에드나 롤링스, 로버타 릭스비', '열다북스', '정치/사회', 'img src', '17')
+
+insert into bookInfo values
+('9791190158022', '젠더는 헤롭다', '쉴라 제프리스', '열다북스', '정치/사회', 'img src', '24')
+
+insert into bookInfo values
+('9791188862290', '지구에서 한아뿐', '정세랑', '난다', '한국 소설', 'img src', '6')
+
+insert into bookInfo values
+('9791166320606', '짐승다운 순정', '태은', '봄미디어', '한국 소설', 'img src', '1')
+
+insert into bookInfo values
+('9791196287801', '며느라기', '수신지', '귤프레스', '만화', 'img src', '16')
+
+insert into bookInfo values
+('9791136252982', '하이큐!!. 44', '하루이치 후루다테', '대원씨아이', '스포츠 만화', 'img src', '2')
+
+insert into bookInfo values
+('9791136721617', '명탐정 코난. 98', '아오야마 고쇼', '서울미디어코믹스(서울문화사)', '추리 만화', 'img src', '5')
+
+insert into bookInfo values
+('9788936478322', '엄마의 마지막 말들', '박희병', '창비', '에세이', 'img src', '8')
+
+insert into bookInfo values
+('9791170400318', '사막에서는 길을 묻지 마라', '나태주', '열림원', '시', 'img src', '5')
+
+insert into bookInfo values
+('9791185401478', '오늘 조금 더 비건', '초식마녀', '채륜서', '에세이', 'img src', '7')
+
+insert into bookInfo values
+('9791189709815', '여자 둘이 살고 있습니다.', '김하나, 황선우', '위즈덤하우스', '에세이', 'img src', '19')
+
+insert into bookInfo values
+('9791155251201' '우리는 서로를 구할 수 있을까', '정지민', '낮은산', '정치/사회', 'img src', '12')
+
+
+insert into member values
+('jimm_02', '이지민', 'jimin0221@naver.com', '01099919992', 'minlee008888!', '1', '3', '서울시 중구 황학동')
+
+insert into member values
+('hdy030', '하도윤', 'hdy030@naver.com', '01088878889', 'dodoyoon8789++', '0', '2', '서울시 종로구 삼청동')
+
+insert into member values
+('ooodkose', '김시우', 'haw98@daum.net', '01055775577', 'woosi**!**', '1', '5', '서울시 종로구 효자동')
+
+insert into member values
+('jww8807', '김주원', 'Jone0708@naver.com', '01000225588', 'one2558jj^^', '0', '1', '서울시 용산구 한남동')
+
+insert into member values
+('hsuP74','박희수', 'parkheesu@gmail.com', '01054260871', 'heesuuu74!!', '1', '4', '서울시 강남구 역삼동')
+
+insert into member values
+('somsomm21','이소민', 'minllee@naver.com', '01099719971', 'minsom*^^*', '1', '3', '서울시 용산구 서빙고동')
+
+insert into member values
+('a_summ', '이은성', 'summ0102@naver.com', '01022442211', '4221summma!', '0', '5', '서울시 마포구 창천동')
+
+insert into member values
+('minkii__0880', '김민기', 'kiki0880@gmail.com', '01088008800', 'mingkikikiki%%', '1', '1', '서울시 성북구 정릉1동')
+
+insert into member values
+('minipp', '이수하', 'pipimin@naver.com', '01033443120', 'pippimin3120!@!', '0', '2', '서울시 관악구 청림동')
+
+insert into member values
+('lim0hi', '정하림', 'hilim@gmail.com', '01044771122', 'clzlsgkfla324~', '0', '2', '서울시 동작구 사당2동')
+
+insert into member values
+('jo_ran_jo', '조애란', 'rani0405@naver.com', '01095174563', 'whdofksWkd^^', '1', '4', '서울시 동작구 노량진1동')
+
+insert into member values
+('yooniin', '김가윤', 'niiny93@daum.net', '01045289763', 'wswhfflek0808!', '0', '3', '서울시 양천구 목5동')
+
+insert into member values
+('meme9006', '박하민', 'memem9006@naver.com', '01090069006', 'memeisfun@$$@', '0', '5', '서울시 영등포구 양평1동')
+
+insert into member values
+('gutenTag', '최하늘', 'guten1003@naver.com', '01010031003', 'hallihallo0303~!', '1', '1', '서울시 구로구 오류1동')
+
+insert into member values
+('kiyako', '고세은', 'jjucin@naver.com', '01002496831', 'hatjjuu88098$%$%', '0', '1', '서울시 구로구 고척2동')
+
+insert into member values
+('umi00imu', '성다은', 'umiimu00@naver.com', '01044513387', 'umissis9006!!@@', '1', '3', '서울시 강남구 도곡1동')
+
+insert into member values
+('suh0shin02', '신준희', 'sjh0825@gmail.com', '01020152016', 'qorkrhvkdy2016!!', '1', '5', '서울시 송파구 잠실6동')
+
+insert into member values
+('k080shil', '김영실', 'k080shil@naver.com', '01008083219', 'rladudtlf0808', '0', '4', '서울시 송파구 오금동')
+
+insert into member values
+('jiy0n9', '윤지영', 'yjy09@gmail.com', '01009453254', 'qnRmfjqsp09**', '1', '2', '서울시 성동구 성수1가1동')
+
+insert into member values
+('choipark', '박최은', 'csliver@naver.com', '01002186621', 'duddnjscjfja?', '1', '2', '서울시 강남구 청담동')
+
+insert into member values
+('stein_min', '최민석', 'mins0523@daum.net', '01005233250', 'wkxhlgkrhtlvek!', '0', '1', '서울시 중랑구 면복2동')
+
+insert into member values
+('j__o__j', '강아솔', 'kangA@naver.com', '01096411469', 'asolasolkang@@', '1', '4', '서울시 광진구 군자동')
+
+insert into member values
+('dumm_dumm', '기나현', 'nanaki@naver.com', '01071714545', 'duaqudqhffu520#', '0', '3', '서울시 동대문구 용신동')
+
+insert into member values
+('80oo08', '안정현', 'annnanna@gmail.com', '01080118110', 'qkdgkr!!808', '0', '5', '서울시 동대문구 답십리2동')
+
+insert into member values
+('nan_chun', '황소윤', 'goodsy@gmail.com', '01093368124', 'ghkdthdbswhsajt$$', '1', '5', '서울시 종로구 혜화동')
+
+insert into member values
+('ramG_p', '박람지', 'rami@naver.com', '01021354789', 'zofflvnjsldk~@@', '1', '2', '서울시 성북구 월곡2동')
+
+insert into member values
+('gokkil', '이곡길', 'musimusi@gmail.com', '01084523697', 'rntlsqkrauf##~', '1', '4', '서울시 중구 신당동')
+
+insert into member values
+('hwidulreo', '나왕눈', 'boranikke@naver.com', '0104011020', 'alrnrzhdlstpxkrth@@', '1', '4', '서울시 중구 약수동')
+
+insert into member values
+('fox__boksu', '김복수', 'boksu@naver.com', '01054632893', 'kimparkBS%^^%', '1', '5', '서울시 종로구 가회동')
+
+insert into member values
+('hair_bosong', '복대지', 'angelDG@naver.com', '01046287539', 'djflsdid??!', '1', '4', '서울시 서대문구 충현동')
+
+insert into member values
+('long_kirin', '김기린', 'longmm@naver.com', '01010295067', 'ahrdlrlsrlfls~~!', '1', '3', '서울시 마포구 서강동')
+
+insert into member values
+('koyo_00', '고요태', 'susanYT@gmail.com', '01078439561', 'tntkstlrekd$!', '1', '3', '서울시 서대문구 홍은2동')
+
+insert into member values
+('Boksil_y', '양복실', 'silsil@naver.com', '01080765983', 'rPfirrhd..!!', '1', '1', '서울시 마포구 상암동')
+
+insert into member values
+('best_ma', '마우수', 'mauss@naver.com', '01006834752', 'ekwoeksmd^^', '1', '5', '서울시 서초구 잠원동')
+
+insert into member values
+('go_rumi', '독고루미', 'dokgo030@naver.com', '01026753984', 'skdmfl~~!66', '1', '5', '서울시 강남구 신사동')
+
+insert into member values
+('U_gang', '새우강', 'moksanom@naver.com', '01055041287', 'Omoklover%%', '1', '2', '서울시 서대문구 천연동')
+
+insert into member values
+('gangchi_00_', '최강치', 'sanho45@naver.com', '01086422486', 'tksghwhtla!@!@', '1', '4', '서울시 관악구 행운동')
+
+insert into member values
+('Kia_zzang', '조극락', 'gwangtal@naver.com', '01036912369', 'qnfTkddodan$$%%', '1', '4', '서울시 동작구 상도1동')
+
+insert into member values
+('mul_mandu', '연제비', 'jEori@naver.com', '01035775311', 'wjddjflckqTkfEjr00!!', '1', '3', '서울시 관악구 은천동')
+
+insert into member values
+('christopher_99', '박불소', 'bulso99@naver.com', '01099141499', 'akdclejrgn@&&@', '1', '4', '서울시 관악구 보라매동')
